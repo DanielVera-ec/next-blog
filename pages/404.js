bca36f484819d4f12cb5dc1322b7c8b97198622b
Daniel Vera <danieelveera@outlook.com>
2021-06-09 18:17:54 -0400

    first commit

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '../components/Layout'
+
+export default function NotFoundPage() {
+    return (
+        <Layout title="not found">
+            <div className="flex flex-col items-center mt-20">
+                <Image src="/images/logo.png" width={70} height={70} className="bg-gray-800 rounded-2xl" />
+                <h1 className="text-6xl my-5">Whoooops!</h1>
+                <h2 className="text-4xl text-gray-400">
+                    THis page doesn't exsist
+                </h2>
+                <button className="bg-indigo-400 text-gray-800 text-2xl font-bold rounded-2xl my-6 w-1/5">
+                    <a href="/">Return Home</a>
+                </button>
+            </div>
+        </Layout>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Layout from "../components/Layout";
+
+export default function ABoutPage() {
+    return (
+        <Layout title="about page">
+            <h1 className="text-5xl border-b-4 pb-5 font-bold">
+                About
+            </h1>
+
+            <div className="bg-white shadow-md rounded-lg px-10 py-6 mt-6">
+                <h3 className="text-2xl mb-5">
+                    Devspace Blog
+                </h3>
+
+                <p className="mb-3">
+                    This is a blog built with tailwindcss and next js and markdown
+                </p>
+
+                <p>
+                    <span className="font-bold">Version 1.0.0</span>
+                </p>
+            </div>
+        </Layout>
+    )
+}
